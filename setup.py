40945a8d1e0d9383e8f89b0e113fdc4477de5207
ls-michal-dabrowski <michalpawel.dabrowski@lightspeedhq.com>
2023-02-06 18:26:37 +0100

    fix gitpython dep version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
         'pytest>=3.5.0',
-        'GitPython>=-3.0.2'],
+        'GitPython>=3.0.2'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Framework :: Pytest',
